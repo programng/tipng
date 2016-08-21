44b70e9f2217f53561649380ab1d6ce7f1249203
Jonathan Ng <jon7ng@gmail.com>
2016-08-21 16:47:27 -0600

    basic implementation

---
+++ tipNg/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  tipNg
+//
+//  Created by Jonathan Ng on 8/18/16.
+//  Copyright © 2016 programNg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let defaultTipIndex = defaults.integerForKey("default_tip_index")
+        tipControl.selectedSegmentIndex = defaultTipIndex
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let defaultTipIndex = defaults.integerForKey("default_tip_index")
+        tipControl.selectedSegmentIndex = defaultTipIndex
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
